cf2471d7a4964d528ad66a9d02208ce49d7e07c2
nuynuy1221 <nuynuy1221@gmail.com>
2026-03-01 00:09:35 +0700

    Update SkipTutorial.lua

--- SkipTutorial.lua
+++ SkipTutorial.lua
@@ -1,15 +1,23 @@
 repeat task.wait() until game:IsLoaded()
 task.wait(5)
 
-local skip = {
+local skip1 = {
+    [1] = "PartTwo",
+    [2] = "Eligible Player Loaded"
+}
+
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip1))
+
+task.wait(1)
+local skip2 = {
     [1] = "PartOne",
     [2] = "Skip"
 }
-game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip))
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip2))
 
 task.wait(1)
-local skip = {
+local skip3 = {
     [1] = "PartTwo",
     [2] = "Skip"
 }
-game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip))
+game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip3))
