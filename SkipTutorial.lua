aff5e11eed27d582cc27a2afb888b8ee4ac55c12
nuynuy1221 <nuynuy1221@gmail.com>
2026-03-11 00:19:21 +0700

    Update SkipTutorial.lua

--- SkipTutorial.lua
+++ SkipTutorial.lua
@@ -1,23 +1,25 @@
 repeat task.wait() until game:IsLoaded()
 task.wait(5)
 
-local skip1 = {
-    [1] = "PartTwo",
-    [2] = "Eligible Player Loaded"
-}
+task.spawn(function()
+    local skip1 = {
+        [1] = "PartTwo",
+        [2] = "Eligible Player Loaded"
+    }
 
-game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip1))
+    game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip1))
 
-task.wait(1)
-local skip2 = {
-    [1] = "PartOne",
-    [2] = "Skip"
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip2))
+    task.wait(1)
+    local skip2 = {
+        [1] = "PartOne",
+        [2] = "Skip"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip2))
 
-task.wait(1)
-local skip3 = {
-    [1] = "PartTwo",
-    [2] = "Skip"
-}
-game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip3))
+    task.wait(1)
+    local skip3 = {
+        [1] = "PartTwo",
+        [2] = "Skip"
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Networking"):WaitForChild("ClientListeners"):WaitForChild("NEWTutorialEvent"):FireServer(unpack(skip3))
+end)
