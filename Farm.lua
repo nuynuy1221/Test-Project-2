4437a385da867223c05924b4b878e7d6f36331bc
nuynuy1221 <nuynuy1221@gmail.com>
2026-03-26 19:11:14 +0700

    Update Farm.lua

--- Farm.lua
+++ Farm.lua
@@ -648,9 +648,9 @@ local function startCustomFarm()
                 wait(3)
             end
 
-            -- ==================== กลับ Lobby หลัง 120 วินาที ====================
-            if elapsed >= 120 then
-                print("🏠 ครบ 120 วินาที → กลับ Lobby")
+            -- ==================== กลับ Lobby หลัง 255 วินาที ====================
+            if elapsed >= 255 then
+                print("🏠 ครบ 255 วินาที → กลับ Lobby")
                 pcall(function()
                     TeleportEvent:FireServer("Lobby")
                 end)
